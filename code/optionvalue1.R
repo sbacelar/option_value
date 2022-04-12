3fd24477c4386f6715bc756b606944c48f469784
Sérgio Bacelar <sergio.bacelar@gmail.com>
2022-04-12 16:22:32 +0100

    3rd commit

--- code/optionvalue1.R
+++ code/optionvalue1.R
@@ -5,21 +5,45 @@ ex <- read_delim("data/stockwise_ex.csv",
                            delim = ";", escape_double = FALSE,
                            trim_ws = TRUE)
 rate = 0.05
-Y <- ex[2]
-B <- ex[4]
+
+Y <- ex$`Earnings Forecast`
+B <- ex$`Incl_SS_Offset`
 t <- ex$Age[1]
 r <- ex$Age[16]
 S <- ex$Age[19]
 
-ov <- tibble()
-calc_opvalue <- function(t, r, S, Y, B, rate) {
+ov1 <- vector("numeric", 15)
+calc_opvalue1 <- function(t, r, S, Y, B, rate) {
   for (i in t:(r-1)){
-    ov <- ov + Y[i-t+1,]/(1+rate)^(i-t)
+    ov1[i-t+1] <- ov1[i-t+1] + Y[i-t+1]/(1+rate)^(i-t)
   }
+  return(sum(ov))
 }
 
-ov <- calc_opvalue(t, r, S, Y, B, rate)
+ov2 <- vector("numeric", 4)
+calc_opvalue2 <- function(t, r, S, Y, B, rate) {
+  for (i in r:S){
+    ov2[i-r+1] <- ov[i-r+1] + B[i-r+1]/(1+rate)^(i-t)
+  }
+  return(sum(ov2))
+}
+
+calc_opvalue1(t, r, S, Y, B, rate)
+calc_opvalue2(t, r, S, Y, B, rate)
 
 for (i in t:(r-1)){
+  print(i)
   print(Y[i-t+1,]/(1+rate)^(i-t))
+}
+
+for (i in r:S){
+  print(B[i-r+1]/(1+rate)^(i-t))
+}
+
+ov2 <- 0
+calc_opvalue2 <- function(t, r, S, Y, B, rate) {
+  for (i in r:S){
+    ov2 <- ov2 + B[i-r+1]/(1+rate)^(i-t)
+  }
+  return(ov2)
 }
